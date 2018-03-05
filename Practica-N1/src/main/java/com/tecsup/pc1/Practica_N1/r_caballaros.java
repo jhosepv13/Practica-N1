ebaf612a4ff3af9aefb4d4918886835ed4b1012c
jhosepv13 <Alumno@LAB701-22>
2018-03-05 11:24:33 -0500

    "Subiendo rama 3"

--- Practica-N1/src/main/java/com/tecsup/pc1/Practica_N1/r_caballaros.java
+++ Practica-N1/src/main/java/com/tecsup/pc1/Practica_N1/r_caballaros.java
@@ -2,4 +2,41 @@ package com.tecsup.pc1.Practica_N1;
 
 public class r_caballaros {
 
+	private String nombre_prenda;
+	private String descripcion;
+	private String talla;
+	private String foto;
+	private double precio;
+	public String getNombre_prenda() {
+		return nombre_prenda;
+	}
+	public void setNombre_prenda(String nombre_prenda) {
+		this.nombre_prenda = nombre_prenda;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getTalla() {
+		return talla;
+	}
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
 }
