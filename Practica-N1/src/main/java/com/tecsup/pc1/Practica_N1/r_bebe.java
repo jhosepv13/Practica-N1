147e8a88fee078cff99addc028c6be95aeb15584
Julio <Alumno@LAB701-22>
2018-03-05 12:05:14 -0500

    "Cambio hecho por Julio"

--- Practica-N1/src/main/java/com/tecsup/pc1/Practica_N1/r_bebe.java
+++ Practica-N1/src/main/java/com/tecsup/pc1/Practica_N1/r_bebe.java
@@ -5,6 +5,24 @@ public class r_bebe {
 	private String b_nombre;
 	private String b_description;
 	private double b_talla;
+	public String getB_nombre() {
+		return b_nombre;
+	}
+	public void setB_nombre(String b_nombre) {
+		this.b_nombre = b_nombre;
+	}
+	public String getB_description() {
+		return b_description;
+	}
+	public void setB_description(String b_description) {
+		this.b_description = b_description;
+	}
+	public double getB_talla() {
+		return b_talla;
+	}
+	public void setB_talla(double b_talla) {
+		this.b_talla = b_talla;
+	}
 
 	
 }
