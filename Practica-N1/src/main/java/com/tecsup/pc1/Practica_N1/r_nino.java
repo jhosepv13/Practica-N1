e17432bac8e823526f8765b6721bb36a1268b976
Alumno <Alumno@LAB701-18>
2018-03-05 11:23:48 -0500

    commit de la rama_4 con la clase r_nino.java

---
+++ Practica-N1/src/main/java/com/tecsup/pc1/Practica_N1/r_nino.java
@@ -0,0 +1,28 @@
+package com.tecsup.pc1.Practica_N1;
+
+public class r_nino {
+
+	private String n_nombre;
+	private String n_foto;
+	private double n_precio;
+	public String getN_nombre() {
+		return n_nombre;
+	}
+	public void setN_nombre(String n_nombre) {
+		this.n_nombre = n_nombre;
+	}
+	public String getN_foto() {
+		return n_foto;
+	}
+	public void setN_foto(String n_foto) {
+		this.n_foto = n_foto;
+	}
+	public double getN_precio() {
+		return n_precio;
+	}
+	public void setN_precio(double n_precio) {
+		this.n_precio = n_precio;
+	}
+	
+	
+}
