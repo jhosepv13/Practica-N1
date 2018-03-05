743db195842b3ac892b95d448ae175c96def0898
Alumno <Alumno@LAB701-13>
2018-03-05 10:55:15 -0500

    "Cambio por Julio Apaza"

---
+++ Practica-N1/src/main/java/com/tecsup/pc1/Practica_N1/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.pc1.Practica_N1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
